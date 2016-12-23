0c2d72bcb927897122cf03e7aa5c338e9946528c
Zephyr <ixuhefeng@gmail.com>
2016-12-23 16:51:39 +0800

    mysql support

--- coupon/makeCoupon.py
+++ coupon/makeCoupon.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import time
+from os import getenv
 from sys import argv
-from random import randint
-from string import uppercase, digits
+from random import choice
+from string import digits, ascii_uppercase as uppercase
+
+import mysql.connector
 from getopt import getopt
 
 
@@ -10,31 +14,55 @@ def makeCoupon(place=5):
     chars = uppercase + digits
     s = ''
     for i in range(place):
-        s += chars[randint(0, len(chars)-1)]
+        s += choice(chars)
     return s
 
 
-def main():
-    place = 5
-    count = 200
+def printThose(count, place):
     result = []
-    opts, args = getopt(argv[1:], "hp:c:", ["help"])
-    for o, a in opts:
-        if o == '-p':
-            place = int(a)
-        elif o == '-c':
-            count = int(a)
-        elif o == '-h' or o == '--help':
-            print '[-p][-c]'
-            exit(0)
     while count > 0:
         s = makeCoupon(place)
         if s not in result:
             result.append(s)
             count -= 1
     for i in result:
-        print i
+        print(i)
 
+
+def mysqlThose(count, place):
+    conn = mysql.connector.connect(user='root',
+                                   password=getenv('mysql_pass'),
+                                   database='Coupon')
+    cursor = conn.cursor(buffered=True)
+    while count > 0:
+        s = makeCoupon(place)
+        sql = 'SELECT Coupon FROM Coupon WHERE Coupon = %s'
+        cursor.execute(sql, (s,))
+        if cursor.fetchall == []:
+            now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+            sql = 'INSERT INTO Coupon (coupon, datetime) values (%s, %s)'
+            cursor.execute(sql, (s, now))
+            count -= 1
+    conn.commit()
+    cursor.close()
+
+
+def main():
+    place = 5
+    count = 20
+    method = printThose
+    opts, args = getopt(argv[1:], "hp:c:", ["help", "mysql"])
+    for o, a in opts:
+        if o == '-p':
+            place = int(a)
+        elif o == '-c':
+            count = int(a)
+        elif o == '--mysql':
+            method = mysqlThose
+        elif o == '-h' or o == '--help':
+            print('[-p][-c]')
+            exit(0)
+    method(count, place)
 
 if __name__ == '__main__':
     main()
